1f1c1d86a94548ffa6f27bf167f1dbe3b090c536
FZambia <frvzmb@gmail.com>
2014-11-20 21:38:40 +0300

    v0.2.1

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def long_description():
 
 setup(
     name='centrifuge-mongodb',
-    version='0.2.0',
+    version='0.2.1',
     description="MongoDB structure backend for Centrifuge",
     long_description=long_description(),
     url='https://github.com/centrifugal/centrifuge-mongodb',
