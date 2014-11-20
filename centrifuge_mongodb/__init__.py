75620f2e5256a77817cd519fcc1c632d9be28bb3
st4lk <myhappydo@gmail.com>
2014-11-20 16:33:28 +0300

    create project name unique index

--- centrifuge_mongodb/__init__.py
+++ centrifuge_mongodb/__init__.py
@@ -110,6 +110,7 @@ class Storage(BaseStorage):
             self._conn.project.drop_indexes()
             self._conn.namespace.drop_indexes()
         self._conn.namespace.ensure_index([('name', 1), ('project_id', 1)], unique=True)
+        self._conn.project.ensure_index("options.name", unique=True)
 
     def connect(self, callback=None):
         self.open_connection()
